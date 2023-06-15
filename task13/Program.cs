25f03f3f4b8a57083a67c5d0a8cda8ec6f616093
MarinLIS <malinalu@mail.ru>
2023-06-15 17:02:32 +0400

    Задача13_Семинар2_вывод третьей цифры числа

---
+++ task13/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine ("Input your number ");
+int number = Convert.ToInt32 (Console.ReadLine());
+if (number < -100) 
+{
+    number = number * (-1);
+}
+if (number>100)
+{
+    if (number < 1000)
+    {
+        int num3 = number % 10;
+        Console.WriteLine("The third figure is " + num3);
+    }
+    else
+    {
+        int num3 = (number / 100 )% 10;
+        Console.WriteLine("The third figure is " + num3);
+    }
+}
+else
+{
+    Console.WriteLine("No third figure, too short number");
+}
