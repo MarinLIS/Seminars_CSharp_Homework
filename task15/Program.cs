1a4c36b738dd7ec976177f4895a0779edc9b3e4e
MarinLIS <malinalu@mail.ru>
2023-06-15 17:12:05 +0400

    Задача15_Семинар2_проверяем выходной или нет

---
+++ task15/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Enter the number of the day of the week ");
+int weekDayNum = Convert.ToInt32 (Console.ReadLine());
+
+if (weekDayNum == 6){Console.WriteLine ("It's Saturday, weekend");}
+else if (weekDayNum == 7){Console.WriteLine ("It's Sunday, weekend");}
+else {Console.WriteLine ("It's NOT weekend");}
