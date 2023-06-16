752298851c3a0b4c96b4f380e0b5c6f753718555
MarinLIS <malinalu@mail.ru>
2023-06-16 17:59:32 +0400

    Задача19_Семинар3_ищем Палиндром

---
+++ task19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+void PalindromNumber (int Num)
+{
+    int rightPartNum = Num % 100;
+    int leftPartNum = Num / 1000;
+    int invNum1 = leftPartNum / 10;
+    int invNum2 = leftPartNum % 10;
+    int invNum22 = rightPartNum / 10;
+    int invNum11 = rightPartNum % 10;
+       if (invNum1 == invNum11 && invNum2 == invNum22) Console.WriteLine ("your Number is Palindrom");
+       else {Console.WriteLine("your number is NOT Palindrom");}
+}
+Console.WriteLine ("Enter your number ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+PalindromNumber (number);
