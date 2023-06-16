87d26df876e8e28db003bc035712ef3b2ee1894d
MarinLIS <malinalu@mail.ru>
2023-06-16 16:57:38 +0400

    Задача23_Семинар3_Куб чисел до N

---
+++ task23/Program.cs
@@ -0,0 +1,31 @@
+﻿void SqurePrinter(int numberN)
+{
+        int current = 1;
+        while (current <= numberN)
+        {
+                double res = Math.Pow(current,3);
+                Console.Write($"{res:f0}, ");
+                current++;
+        }
+        Console.WriteLine("\b\b.");
+}
+
+void SqurePrinterNeg(int numberN) // на случай, если отрицательные
+{
+        int current = -1;
+        while (current >= numberN)
+        {
+                double res = Math.Pow(current,3);
+                Console.Write($"{res:f0}, ");
+                current--;
+        }
+        Console.WriteLine("\b\b.");
+}
+
+Console.Write("Input number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+if (user_number > 0)
+        SqurePrinter(user_number);
+else
+        SqurePrinterNeg(user_number);
