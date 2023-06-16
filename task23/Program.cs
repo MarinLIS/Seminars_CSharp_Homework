59e40abe10e6755bcbba9125701479dd946f8a42
MarinLIS <malinalu@mail.ru>
2023-06-16 16:59:02 +0400

    Задача23_Семинар3_Куб чисел до N

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,4 +1,9 @@
-﻿void SqurePrinter(int numberN)
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void SqurePrinter(int numberN)
 {
         int current = 1;
         while (current <= numberN)
