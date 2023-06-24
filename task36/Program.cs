9087d81b71633642b1596b896d9b25218bd4828e
MarinLIS <malinalu@mail.ru>
2023-06-24 23:33:35 +0400

    Задача про начйти разницу макс и мин вещественных элементов масива

---
+++ task36/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int [] CreateArray ()
+{
+    Console.WriteLine ("Enter the array volume");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int [] CreateArray = new int [size];
+    for (int i =0; i < size; i++)
+    {
+        CreateArray[i]= new Random().Next (-10, 10);
+    }
+    return CreateArray;
+}
+
+void PrintArray (int [] arrayNew)
+
+{
+    for (int i = 0; i < arrayNew.Length; i++)
+    {
+        Console.Write(arrayNew[i] + " ");
+    }
+}
+
+void EvenCounter (int [] myArray)
+{
+    int count = 0;
+    for (int i = 1; i < myArray.Length; i=i+2)
+    {
+        count ++;
+    }
+    Console.WriteLine($" -> {count} elements with not-even indices");
+}
+int [] random = CreateArray();
+PrintArray (random);
+EvenCounter(random);
+
---
+++ task38/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double [] RandomArray ( )
+{
+    int size = 12;
+    double [] RandomArray = new double [size];
+    for ( int i=0; i < size; i++)
+    {
+        RandomArray[i] = new Random().NextDouble() *10; 
+    }
+    return RandomArray;
+}
+
+
+void ShowArray (double [] arrayToShow)
+{
+    for (int i =0; i < arrayToShow.Length; i++)
+        Console.Write ($"{arrayToShow[i]:f2}, ");
+    Console.WriteLine("\b\b ");
+}
+
+void NumDif (double [] arrayDif)
+{
+    double maxVal = 0;
+    double elemDiff = 0;
+    for (int i = 0; i < arrayDif.Length; i++)
+    {
+        if (arrayDif[i]> maxVal)
+        {
+            maxVal = arrayDif[i];
+        }
+    }
+    Console.Write($"{maxVal:f2} - ");
+
+    double minVal = arrayDif[0];
+    for (int i = 0; i < arrayDif.Length; i++)
+    {
+        if (arrayDif[i]< minVal)
+        {
+            minVal = arrayDif[i];
+        }
+    }
+    Console.Write ($"{minVal:f2}");
+elemDiff = maxVal - minVal;
+Console.WriteLine($" = > {elemDiff:f2}");
+}
+
+double [] arrayRand = RandomArray();
+ShowArray (arrayRand);
+NumDif (arrayRand);
+
