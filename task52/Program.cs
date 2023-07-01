7da810cf18af0c2bc777d59cd48f09de10497fb4
MarinLIS <malinalu@mail.ru>
2023-07-02 00:23:57 +0400

    Задача вычислить среднее ариф. столбцов 2В массива

---
+++ task52/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int [,] New2DArray ()
+{
+    Console.WriteLine("Enter dimension for square array: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+            
+    int [,] newArray = new int [n, n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j< n; j++)
+        {
+            newArray[i,j] = new Random().Next(1, 10);
+        }
+    }
+    return newArray;
+}
+
+void ArrayToPrint (int [,] array2dToPrint)
+{
+    for (int i = 0; i < array2dToPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2dToPrint.GetLength(1); j++)
+        {
+            Console.Write(array2dToPrint[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void PrintAvg (int [,] array)
+{
+    int n = array.GetLength(0);
+    for (int i = 0; i < n; i++)
+        {
+            double avgSum = 0;
+            double count =0;
+            for (int j = 0; j < n; j++)
+            {
+                count+= array[j, i];
+                avgSum = Math.Round((count / n), 2);
+            }
+            Console.WriteLine("Sum in {0} column: {1}", i, avgSum);
+        }
+}
+    
+
+
+int [,] array = New2DArray();
+ArrayToPrint (array);
+PrintAvg (array);
