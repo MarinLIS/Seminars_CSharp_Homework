6c3824f53772f1bb0b1efc25cf7ea2a7eccb507a
MarinLIS <malinalu@mail.ru>
2023-07-02 22:13:57 +0400

    Задача создатьдвумерный вещественный  массив с Random

--- task47/Program.cs
+++ task47/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
 
 // m = 3, n = 4.
 // 0,5 7 -2 -0,2
@@ -6,26 +7,28 @@
 // 8 7,8 -7,1 9
 
 
-int [,] New2DArray ()
+double [,] New2DArray ()
 {
     Console.WriteLine ("Enter the quontity of rows: ");
     int numRows = Convert.ToInt32 (Console.ReadLine());
     Console.WriteLine ("Enter the quontity of collumns: ");
     int numColls = Convert.ToInt32 (Console.ReadLine());
 
-    int [,] newArray = new int [numRows, numColls];
+    double [,] newArray = new double [numRows, numColls];
 
     for (int i = 0; i < numRows; i++)
     {
         for (int j = 0; j< numColls; j++)
         {
-            newArray[i,j] = new Random().Next(-40,41);
+            newArray[i,j] = Math.Round((new Random().Next(-100,100)*0.1),2);
+                // (new Random().NextDouble()*100), 2); - можно и так, 
+                // но тогда в массиве вещественные положительные
         }
     }
     return newArray;
 }
 
-void ArrayToPrint (int [,] array2dToPrint)
+void ArrayToPrint (double [,] array2dToPrint)
 {
     for (int i = 0; i < array2dToPrint.GetLength(0); i++)
     {
@@ -38,5 +41,5 @@ void ArrayToPrint (int [,] array2dToPrint)
     Console.WriteLine();
 }
 
-int [,] array2D = New2DArray();
+double [,] array2D = New2DArray();
 ArrayToPrint(array2D);
